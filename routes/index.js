5621c18ae6ebe916a006077f3f3f96ad43c7969c
Dreyke <25332661+Dreyke@users.noreply.github.com>
2018-04-10 17:54:37 -0500

    Update to slide 50

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var apodService = require('../services/apod');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('fetchpicture');
 });
 
 
 /* Fetch a picture from NASA's Astronomy Picture of the Day service */
 router.get('/fetchpicture', function (req, res, next) {
 
-  if (req.query.picturetype === 'random') {
-    res.send('todo: get random picture');
-  }
-  else {
-    //default is today's picture
-      res.send('todo: get today\'s picture');
-  }
+    // If random picture requested, fetch random picture
+    // Otherwise fetch today's picture
+
+    apodService(function (err, apod_data) {
+
+      if (err) {
+        res.render('apod_error', {message: err.message, title: 'Error'});
+      } else {
+        res.render('index', {apod: apod_data, title: `APOD for ${apod_data.date}`});
+      }
+
+    }, req.query.picturetype);
 
 });
 
